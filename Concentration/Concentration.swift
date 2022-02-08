b495dceab01a25f83a9fab6881ac86b6bef17d9b
orenCPC <oren@cherrypick-consulting.com>
2022-02-08 16:35:48 +0200

    Code from Lectures 1 and 2

---
+++ Concentration/Concentration.swift
@@ -0,0 +1,47 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Oren Dinur on 08/02/2022.
+//
+
+import Foundation
+
+class Concentration{
+    
+    var cards = Array <Card>()
+    //Same as [Card]()
+    
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index: Int){
+        if !cards[index].isMatched{
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                //check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                //either no cards or 2 cards are faced up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+        
+    }
+    
+    init(numberOfPairsOfCards: Int){
+        for _ in 1...numberOfPairsOfCards{
+            let card = Card()
+            cards += [card,card]
+        }
+        
+        // TODO: Shuffle the cards
+    }
+}
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -8,11 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
  
     var flipCount = 0 {
         didSet{
             flipCountLabel.text = "Flips: \(flipCount)"
-
         }
     }
     
@@ -20,25 +21,57 @@ class ViewController: UIViewController {
     @IBOutlet var cardButtons: [UIButton]!
     
     @IBOutlet weak var flipCountLabel: UILabel!
-    
+        
     @IBAction func touchCard(_ sender: UIButton) {
     flipCount += 1
-        
-    }
-    
-   
-    
-    
-    func flipCard(withEmoji emoji: String, on button: UIButton){
-        print("Click")
-        if button.currentTitle == emoji {
-            button.setTitle("", for: UIControl.State.normal)
-            button.backgroundColor = UIColor.orange
-        }else {
-            button.setTitle(emoji, for: UIControl.State.normal)
-            button.backgroundColor = UIColor.white
+        if let cardNumber = cardButtons.firstIndex(of: sender){
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
+        } else{
+            print("Chosen card wasn't in cardButtons")
         }
     }
     
+    func updateViewFromModel(){
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp{
+                button.setTitle(emoji(for: card), for: UIControl.State.normal)
+                button.backgroundColor = UIColor.white
+            } else {
+                button.setTitle("", for: UIControl.State.normal)
+                button.backgroundColor = card.isMatched ? UIColor.clear : UIColor.orange
+            }
+        }
+    }
+    
+    var emojiChoices = ["😀","👻","🎃","👸🏽","🥸","🥴","🤟🏽","🤜🏼"]
+    
+    //Dictionary
+    var emoji = [Int:String]()
+    
+    func emoji(for card : Card) -> String {
+        if emoji[card.identifier] == nil {
+            if emojiChoices.count > 0 {
+                let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+                emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+        }
+    }
+        
+        
+        //2 Ways to handle the optional:
+        
+//        if emoji[card.identifier] != nil{
+//            return emoji[card.identifier]!
+//        } else {
+//            return "?"
+//        }
+        
+        return emoji[card.identifier] ?? "?"
+    }
+    
+    
 }
+
 
